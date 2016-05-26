9e3c1adc2494e59c3f4d29f568be41b014c5bc8d
Niklas Hedlund <nojan1989@gmail.com>
2016-05-26 18:41:35 +0200

    Prep

---
+++ API.Tests/MatchupTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API.Tests
+{
+    [TestClass]
+    public class MatchupTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
